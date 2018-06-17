1ce0c4e96d9df3dd9311e25f5348fb6d2eff1f1d
Kataev Vitaliy <vita@kataev.pro>
2018-06-17 20:08:12 +0300

    Implement markup and Bookmarks top bar - Markup Layout - Markup Jira, GitHub, Wunderlist panels.

--- extension/src/app/app.module.ts
+++ extension/src/app/app.module.ts
@@ -4,16 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { WidgetComponent } from './widget/widget.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { PullRequestComponent } from './pull-request/pull-request.component';
+import { JiraComponent } from './jira/jira.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { BookmarkDropdownComponent } from './bookmark-dropdown/bookmark-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetComponent,
+    PullRequestComponent,
+    JiraComponent,
+    BookmarksComponent,
+    BookmarkDropdownComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    NgbModule.forRoot(),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
